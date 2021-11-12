993baf8bbff1a51de3afd6e9598f28f299e8710c
Masoud RAVANDOUST <57909214+MRavandoust@users.noreply.github.com>
2021-11-12 13:41:05 +0100

    Index and contact pages add

--- DevCard_project/DevCard_project/Controllers/HomeController.cs
+++ DevCard_project/DevCard_project/Controllers/HomeController.cs
@@ -11,22 +11,18 @@ namespace DevCard_project.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
